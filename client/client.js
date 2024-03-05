ea16b9515b4b069b2c7a9a1119737b90f6f683e1
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-05 12:00:50 +0530

    electron config

---
+++ client/client.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    icon: "./public/icons/.ico/x32.ico",
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.removeMenu();
+  win.loadURL('http://localhost:3000/');
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    console.log('App closed!');
+    app.quit()
+  }
+})
