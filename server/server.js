47eca952714dfee548d45a74e07bb5214868b738
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-05 12:22:36 +0530

    Update server.js

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express'),
+      app = express(),
+      PORT = 6969;
+
+require('dotenv').config();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.static('public'));
+
+app.get("/", (req, res) =>{
+    res.render('main')
+});
+
+app.listen(PORT || process.env.PORT, process.env.IP, () => {
+    console.log('xpotify server is up on PORT: ' + PORT);
+});
