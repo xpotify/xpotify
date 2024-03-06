0955f8dee2dd88c146dd5143d610a667d3bad559
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-07 00:42:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static('public'));
 
 app.get("/", (req, res) =>{
-    res.render('main')
+    res.render('home');
 });
 
 app.listen(PORT || process.env.PORT, process.env.IP, () => {
