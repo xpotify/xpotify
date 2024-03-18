a4c4d1a5d2c85d3f07671ae69e4a803fbc5e67f5
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-18 19:55:03 +0530

    UI reaction functions created

--- server/public/script/sections.js
+++ server/public/script/sections.js
@@ -1,12 +1,21 @@
 const artistTab = document.getElementById("artistTabs");
 const tabs = document.getElementById("tabs");
+const songTabs = document.getElementById("songsTabs");
 
-const toggleArtistTab = () => {
-    tabs.classList.toggle("hide");
-    artistTab.classList.toggle("hide");
+const showArtistTab = () => {
+    tabs.classList.add("hide");
+    artistTab.classList.remove("hide");
+    songTabs.classList.add("hide");
 };
 
 const returnToHome = () => {
     artistTab.classList.add("hide");
     tabs.classList.remove("hide");
+    songTabs.classList.add("hide");
+};
+
+const showSongTab = () => {
+    artistTab.classList.add("hide");
+    tabs.classList.add("hide");
+    songTabs.classList.remove("hide");
 };
