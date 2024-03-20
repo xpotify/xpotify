b9c309d46a4d755477c88d337ed7335002eb06c1
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-20 09:54:02 +0530

    albumTab added

--- server/public/script/sections.js
+++ server/public/script/sections.js
@@ -1,21 +1,32 @@
 const artistTab = document.getElementById("artistTabs");
 const tabs = document.getElementById("tabs");
 const songTabs = document.getElementById("songsTabs");
+const albumTabs = document.getElementById("albumTabs");
 
 const showArtistTab = () => {
     tabs.classList.add("hide");
     artistTab.classList.remove("hide");
     songTabs.classList.add("hide");
+    albumTabs.classList.add("hide");
 };
 
 const returnToHome = () => {
     artistTab.classList.add("hide");
     tabs.classList.remove("hide");
     songTabs.classList.add("hide");
+    albumTabs.classList.add("hide");
 };
 
 const showSongTab = () => {
     artistTab.classList.add("hide");
     tabs.classList.add("hide");
     songTabs.classList.remove("hide");
+    albumTabs.classList.add("hide");
+};
+
+const showAlbumTab = () => {
+    artistTab.classList.add("hide");
+    tabs.classList.add("hide");
+    songTabs.classList.add("hide");
+    albumTabs.classList.remove("hide");
 };
