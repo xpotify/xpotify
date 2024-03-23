4feb0de868a7b55b8f8415937c22b4a51ae0145d
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-23 17:17:34 +0530

    created modular route handlers

--- server/server.js
+++ server/server.js
@@ -2,21 +2,21 @@ const express = require('express'),
       app = express(),
       PORT = 6969;
 
+const artist = require("./routes/artistRoutes");
+const song = require("./routes/songRoutes");
+const playlist = require("./routes/playlistRoutes");
+const indexRoutes = require("./routes/indexRoutes");
 require('dotenv').config();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.static('public'));
-
-app.get("/", (req, res) =>{
-    res.render('home');
-});
-
-app.get("/serv", (req, res) => {
-    const user = {"name": "john", "age": "14"};
-    res.json(user);
-});
+app.use(indexRoutes);
+app.use("/artist", artist);
+// app.use("/playlist", playlist);
+// app.use("/song", song);
 
 app.listen(PORT || process.env.PORT, process.env.IP, () => {
     console.log('xpotify server is up on PORT: ' + PORT);
 });
+
