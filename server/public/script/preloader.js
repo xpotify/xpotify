f02e50fd37edef85cab3f134275ce1990fd1be4c
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-28 04:30:38 +0530

    loader bug fixes

--- server/public/script/preloader.js
+++ server/public/script/preloader.js
@@ -1,17 +1,10 @@
 const loader = document.getElementById("preloader");
 const webLoader = document.getElementById("webLoader");
-
-const preloader = (tab) => {
-    loader.classList.remove("hide");
-    tabs.classList.add("hide");
-    // window.addEventListener("load", () => {
-    //     console.log("loaded");
-    //     loader.classList.add("hide");
-    //     playlistTabs.classList.remove("hide");
-    // });
-};
+const prMetric = document.getElementById("preloaderMetricIMG");
 
 window.addEventListener("load", () => {
     webLoader.classList.add("hide");
     tabs.classList.remove("hide");
 });
+
+
