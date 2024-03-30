2ddcbf2163c34f0ece0aa86be355177b05896f30
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-30 14:40:24 +0530

    Update sections.js

--- server/public/script/sections.js
+++ server/public/script/sections.js
@@ -5,47 +5,56 @@ const albumTabs = document.getElementById("albumTabs");
 const lyricsTabs = document.getElementById("lyricsTab");
 const playlistTabs = document.getElementById("playlistTabs");
 
-// const showArtistTab = () => {
-//     tabs.classList.add("hide");
-//     artistTab.classList.remove("hide");
-//     songTabs.classList.add("hide");
-//     albumTabs.classList.add("hide");
-//     lyricsTabs.classList.add("hide");
-//     playlistTabs.classList.add("hide");
-// };
+const showArtistTab = () => {
+        tabs.classList.add("hide");
+        artistTab.classList.remove("hide");
+        songTabs.classList.add("hide");
+        albumTabs.classList.add("hide");
+        lyricsTabs.classList.add("hide");
+        playlistTabs.classList.add("hide");
+};
 
-// const returnToHome = () => {
-//     artistTab.classList.add("hide");
-//     tabs.classList.remove("hide");
-//     songTabs.classList.add("hide");
-//     albumTabs.classList.add("hide");
-//     lyricsTabs.classList.add("hide");
-//     playlistTabs.classList.add("hide");
-// };
+const showList = () => {
+        tabs.classList.add("hide");
+        artistTab.classList.add("hide");
+        songTabs.classList.add("hide");
+        albumTabs.classList.add("hide");
+        lyricsTabs.classList.add("hide");
+        playlistTabs.classList.remove("hide");    
+};
 
-const showSongTab = () => {
+const returnToHome = () => {
     artistTab.classList.add("hide");
-    tabs.classList.add("hide");
-    songTabs.classList.remove("hide");
+    tabs.classList.remove("hide");
+    songTabs.classList.add("hide");
     albumTabs.classList.add("hide");
     lyricsTabs.classList.add("hide");
     playlistTabs.classList.add("hide");
+};
+
+const showSongTab = () => {
+        artistTab.classList.add("hide");
+        tabs.classList.add("hide");
+        songTabs.classList.remove("hide");
+        albumTabs.classList.add("hide");
+        lyricsTabs.classList.add("hide");
+        playlistTabs.classList.add("hide");
 };
 
 const showAlbumTab = () => {
-    artistTab.classList.add("hide");
-    tabs.classList.add("hide");
-    songTabs.classList.add("hide");
-    albumTabs.classList.remove("hide");
-    lyricsTabs.classList.add("hide");
-    playlistTabs.classList.add("hide");
+        artistTab.classList.add("hide");
+        tabs.classList.add("hide");
+        songTabs.classList.add("hide");
+        albumTabs.classList.remove("hide");
+        lyricsTabs.classList.add("hide");
+        playlistTabs.classList.add("hide");
 };
 
 const showLyricsTab = () => {
-    artistTab.classList.add("hide");
-    tabs.classList.add("hide");
-    songTabs.classList.add("hide");
-    albumTabs.classList.add("hide");
-    lyricsTabs.classList.remove("hide");
-    playlistTabs.classList.add("hide");
+        artistTab.classList.add("hide");
+        tabs.classList.add("hide");
+        songTabs.classList.add("hide");
+        albumTabs.classList.add("hide");
+        lyricsTabs.classList.remove("hide");
+        playlistTabs.classList.add("hide");
 };
