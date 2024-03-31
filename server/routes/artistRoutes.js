15e63370b7f72de0b5ed255c99f4061e66d46cbf
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-31 13:29:43 +0530

    artist API created

--- server/routes/artistRoutes.js
+++ server/routes/artistRoutes.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+require('dotenv').config();
+const { Client } = require("spotify-api.js");
+const cli = new Client({ 
+    token: { clientID: process.env.cId, clientSecret: process.env.cSecret}
+});
 
-router.get("/", (req, res) => {
-    res.send("HAHA");
+const getArtist = async () => {
+    const data = await cli.artists.search("alan walker");
+    return data
+};
+
+// const data = getTracksFromPlaylist("1JXmQxcfIdcKnU7gNw9tbP");
+
+router.get("/q/:id", async (req, res) => {
+    const query = await getArtist();
+    // console.log(query);
+    res.json(query);
 });
 
 module.exports = router;
