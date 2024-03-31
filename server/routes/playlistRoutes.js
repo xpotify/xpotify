64d7133138f27d63075203c34f7e3023d3a62fa6
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-03-31 13:29:46 +0530

    Update playlistRoutes.js

---
+++ server/routes/playlistRoutes.js
@@ -0,0 +1,16 @@
+// getTracksFromPlaylist = async function(playlistId){
+//     const tracks = await cli.playlists.getTracks(playlistId);
+//     var track = [];
+//     for(i=0, x=1; i < tracks.length; i++,x++){
+//             var gotTrack = {
+//             id: x,
+//             name: tracks[i].track.name,
+//             artist: tracks[i].track.artists[0].name,
+//             ytQuery: tracks[i].track.name + " - " + tracks[i].track.artists[0].name
+//         };
+//         track.push(gotTrack);
+//     };
+//     return track;
+// };
+
+// // const data = getTracksFromPlaylist("1JXmQxcfIdcKnU7gNw9tbP");
