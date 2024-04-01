e186d8d4bd66e6746cb565a42c7140113bb141de
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-04-01 18:31:57 +0530

    playlist Fetch routes

--- server/routes/playlistRoutes.js
+++ server/routes/playlistRoutes.js
@@ -1,16 +1,56 @@
-// getTracksFromPlaylist = async function(playlistId){
-//     const tracks = await cli.playlists.getTracks(playlistId);
-//     var track = [];
-//     for(i=0, x=1; i < tracks.length; i++,x++){
-//             var gotTrack = {
-//             id: x,
-//             name: tracks[i].track.name,
-//             artist: tracks[i].track.artists[0].name,
-//             ytQuery: tracks[i].track.name + " - " + tracks[i].track.artists[0].name
-//         };
-//         track.push(gotTrack);
-//     };
-//     return track;
-// };
+const express = require("express");
+const router = express.Router();
+require('dotenv').config();
 
-// // const data = getTracksFromPlaylist("1JXmQxcfIdcKnU7gNw9tbP");
+const { Client } = require("spotify-api.js");
+const cli = new Client({ 
+    token: { clientID: process.env.cId, clientSecret: process.env.cSecret}
+});
+
+const getPlaylist = async (id) => {
+    const data = await cli.playlists.get(id);
+    return data;
+};
+
+const getTracks = async (id) => {
+    const data = await cli.playlists.getTracks(id);
+    return data;
+};
+
+router.get("/q/:id", async (req, res) => {
+    const query = await getPlaylist(req.params.id);
+    const playlist = {
+        "id" : query.id,
+        "image" : query.images[0].url,
+        "name" : query.name,
+        "owner" : {
+            "name" : query.owner.displayName,
+            "id" : query.owner.id
+        },
+        "totalTracks" : query.totalTracks
+
+    }
+    // console.log(query);
+    res.json(playlist);
+});
+
+router.get("/q/tracks/:id", async (req, res) => {
+    const response = await getTracks(req.params.id);
+    var tracks = [];
+    for(i=0, x=1; i < response.length; i++,x++){
+        var gotTrack = {
+        "id" : x,
+        "songId" : response[i].track.id,
+        "name" : response[i].track.name,
+        "artist" : response[i].track.artists,
+        "ytQuery" : response[i].track.name + " - " + response[i].track.artists[0].name,
+        "albumId" : response[i].track.album.id,
+        "images" : response[i].track.album.images
+    };
+    tracks.push(gotTrack);
+};
+    // console.log(query);
+    res.json(tracks);
+});
+
+module.exports = router;
