c580ef3bf17cb4574db9d70b519c6f6598422388
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-04-01 18:32:00 +0530

    Update indexRoutes.js

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -5,10 +5,6 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
-router.get('/about', (req, res) => {
-  res.send('About birds');
-});
-
 router.get("/serv", (req, res) => {
     const user = {"name": "john", "age": "14"};
     res.json(user);
