208beecf5896a830a5271a5cb10489ba55d291c0
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-04-01 18:34:03 +0530

    Fetch API updates

--- server/public/script/fetch.js
+++ server/public/script/fetch.js
@@ -1,5 +1,5 @@
-const fetchAPI = async () => {
-    const response = await fetch("http://localhost:6969/artist/q/alanwalker", {
+const fetchArtist = async (id) => {
+    const response = await fetch(`http://localhost:6969/artist/q/${id}`, {
         method: "GET", 
         mode: "cors",
         headers: {
@@ -7,7 +7,33 @@ const fetchAPI = async () => {
         }
         });
         
-        const data = await response.json();
+    const data = await response.json();
 
-        console.log(data);
+    console.log(data);
+};
+
+const fetchPlaylist = async (id) => {
+    const response = await fetch(`http://localhost:6969/playlist/q/${id}`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    });
+
+    const data = await response.json();
+    console.log(data);
+};
+
+const fetchTracks = async (id) => {
+    const response = await fetch(`http://localhost:6969/playlist/q/tracks/${id}`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    });
+
+    const data = await response.json();
+    console.log(data);
 };
