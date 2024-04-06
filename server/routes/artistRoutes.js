8161e7bba93f3333373c14d4611697bab929863b
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-04-06 17:48:53 +0530

    Update artistRoutes.js

--- server/routes/artistRoutes.js
+++ server/routes/artistRoutes.js
@@ -17,4 +17,34 @@ router.get("/q/:id", async (req, res) => {
     res.json(query);
 });
 
+const getTopTracks = async (id) => {
+    const data = await cli.artists.getTopTracks(id);
+    return data;
+};
+
+router.get("/toptracks/:id", async (req, res) => {
+    const response = await getTopTracks(req.params.id);
+    var tracks = [];
+
+    for(i=0, x=1; i < response.length; i++, x++){
+        var gotTrack = {
+            "id" : x,
+            "artists" : response[i].artists,
+            "duration" : response[i].duration,
+            "songId" : response[i].id,
+            "name" : response[i].name,
+            "album" : {
+                "name" : response[i].album.name,
+                "id" : response[i].album.id,
+                "img" : response[i].album.images
+            }
+        }
+
+        tracks.push(gotTrack);
+    }
+
+    // console.log(query);
+    res.json(tracks);
+});
+
 module.exports = router;
