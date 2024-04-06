b6c7c2462155ba4af73bd07c4d0ad8d99e4ab471
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-04-06 19:51:16 +0530

    artist APIs

--- server/public/script/sections.js
+++ server/public/script/sections.js
@@ -4,14 +4,49 @@ const songTabs = document.getElementById("songsTabs");
 const albumTabs = document.getElementById("albumTabs");
 const lyricsTabs = document.getElementById("lyricsTab");
 const playlistTabs = document.getElementById("playlistTabs");
+const Loader = document.getElementById("preloader");
 
-const showArtistTab = () => {
+const showArtistTab = async () => {
         tabs.classList.add("hide");
-        artistTab.classList.remove("hide");
         songTabs.classList.add("hide");
         albumTabs.classList.add("hide");
         lyricsTabs.classList.add("hide");
         playlistTabs.classList.add("hide");
+        Loader.classList.add("hide");
+        artistTab.classList.remove("hide");
+        
+        var artistQuery = document.getElementById("songArtist").innerText;
+        var artist = await fetchArtist(artistQuery);
+        var artistId = artist.id;
+        var artistTopTracks = await fetchArtistTopTracks(artistId);
+        
+        if(artist && artistTopTracks){
+                let artistName = document.getElementById("artistName");
+                let artistVerification = document.getElementById("artistVer");
+                let artistFollowers = document.getElementById("artistFollowers");
+                let artistTracks = document.getElementsByClassName("artistTopTracks");
+
+                artistName.innerText = artist.name;
+                artistFollowers.innerText = `${artist.totalFollowers} total followers`;
+
+                if(artist.popularity > 50){
+                        artistVerification.style.visibility = "visible";
+                } else {
+                        artistVerification.style.visibility = "hidden";
+                }
+
+                for(i=0; i <= artistTracks.length; i++){
+                        artistTracks[i].children[0].innerText = artistTopTracks[i].id;
+                        artistTracks[i].children[1].children[0].currentSrc = artistTopTracks[i].album.img[2].url;
+                        artistTracks[i].children[2].innerText = artistTopTracks[i].name;
+                        // artistTopTracks[i].childer[3].
+                        // artistTopTracks[i].childer[4].
+                        artistTracks[i].children[5].innerText = artistTopTracks[i].album.name;
+                }
+
+        } else {
+                console.log("artist doesnt exist!");
+        }
 };
 
 const showList = () => {
