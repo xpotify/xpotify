ad8cff546fcca7ad2df0d73431c16e468c529d80
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-04-09 22:45:15 +0530

    Update songRoutes.js

--- server/routes/songRoutes.js
+++ server/routes/songRoutes.js
@@ -11,10 +11,43 @@ const getTopTracks = async (id) => {
     return data;
 };
 
+const getTrack = async (id) => {
+    const data = await cli.tracks.get(id);
+    return data;
+};
+
 router.get("/gettoptracks/:id", async (req, res) => {
     const query = await getTopTracks(req.params.id);
     // console.log(query);
     res.json(query);
+});
+
+router.get("/gettrack/:id", async (req, res) => {
+    const response = await getTrack(req.params.id);
+    var data = 
+        {
+            "artists" : response.artists,
+            "discNumber":  response.discNumber,
+            "duration" : response.duration,
+            "explicit" : response.explicit,
+            "url" : response.externalURL.spotify,
+            "id" : response.id,
+            "name" : response.name,
+            "type" : response.type,
+            "album" : {
+                "artists" : response.album.artists,
+                "id" : response.album.id,
+                "images" : response.album.images,
+                "name" : response.album.name,
+                "realease-date" : response.album.releaseDate,
+                "totalTracks" : response.album.totalTracks,
+                "type" : response.album.type
+            },
+            "popularity" : response.popularity
+        }
+    ;
+    
+    res.json(data);
 });
 
 module.exports = router;
