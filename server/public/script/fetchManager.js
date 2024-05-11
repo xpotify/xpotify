6df354b32e6cc6b74a2200adc0ca1800400824d0
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-05-11 19:55:26 +0530

    Delete fetchManager.js

--- server/public/script/fetchManager.js
+++
@@ -1,8 +0,0 @@
-const getArtist = async (artistName) => {
-    var artist = await fetchArtist(artistName);
-    let artistId = artist[0].id;
-    var artistTopTracks = await fetchArtistTopTracks(artistId);
-
-    console.log(artist[0], artistTopTracks);
-    return artist[0], artistTopTracks;
-};
