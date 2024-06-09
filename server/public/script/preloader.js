b9cebef8acbd82a637476f1e7e5118415429ca5f
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-06-09 17:38:55 +0530

    Greet update

--- server/public/script/preloader.js
+++ server/public/script/preloader.js
@@ -4,5 +4,28 @@ const prMetric = document.getElementById("preloaderMetricIMG");
 
 window.addEventListener("load", () => {
     webLoader.classList.add("hide");
+
+    const greetMsg = document.getElementById("greetMsg");
+    const greetImage = document.getElementById("greetImage");
+
+    let date = new Date;
+    let time = date.getHours();
+
+    if(3 <= time && time < 12){
+            greetMsg.innerText = "Good Morning,";
+            greetImage.src = "/icons/Morning.png";
+            greetImage.classList.toggle("morning");
+    } else if(12 < time && time <= 18){
+            greetMsg.innerText = "Good Afternoon,";
+            greetImage.src = "/icons/sunset.png";
+            greetImage.classList.toggle("sunset");
+    } else if(18 < time && time < 3){
+            greetMsg.innerText = "Good Evening,";
+            greetImage.src = "/icons/moon.png";
+            greetImage.classList.toggle("night");
+    } else {
+            console.log("Invalid Time.");
+    }
+
     tabs.classList.remove("hide");
 });
