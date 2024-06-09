fa7d7c0d81d22f6bbc5729f9403d0d0902c8cbb0
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-06-09 22:56:51 +0530

    Update preloader.js

--- server/public/script/preloader.js
+++ server/public/script/preloader.js
@@ -11,7 +11,9 @@ window.addEventListener("load", () => {
     let date = new Date;
     let time = date.getHours();
 
-    if(3 <= time && time < 12){
+    console.log(time);
+
+    if(1 <= time && time < 12){
             greetMsg.innerText = "Good Morning,";
             greetImage.src = "/icons/Morning.png";
             greetImage.classList.toggle("morning");
@@ -19,7 +21,7 @@ window.addEventListener("load", () => {
             greetMsg.innerText = "Good Afternoon,";
             greetImage.src = "/icons/sunset.png";
             greetImage.classList.toggle("sunset");
-    } else if(18 < time && time < 3){
+    } else if(18 < time && time < 24){
             greetMsg.innerText = "Good Evening,";
             greetImage.src = "/icons/moon.png";
             greetImage.classList.toggle("night");
