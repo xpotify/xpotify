935fbeedd789071ccc3ec937e1d5c4eaf57b4b63
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-06-17 22:29:23 +0530

    Update indexRoutes.js

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+
+
+const downloadSong = (id, trackName) => {
+    const videoId = `https://www.youtube.com/watch?v=${id}`;
+    console.log("fn triggered!");
+    const download = ytdl(videoId, { filter: "audioonly", quality: "highestaudio"});
+    const stream = fs.createWriteStream(`./public/songs/${trackName}.mp3`);
+
+    download.pipe(stream).on("finish", () => {
+        console.log("Track has been downloaded!");
+    });
+};
 
 router.get('/', (req, res) => {
   res.render('home');
+});
+
+router.post("/:id/:trackname", (req, res) => {
+  downloadSong(req.params.id, req.params.trackname);
 });
 
 module.exports = router;
