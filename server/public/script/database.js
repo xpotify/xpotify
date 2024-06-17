d6c0e1a8ffdb646e32195cdc6cea3439dc477701
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-06-17 16:14:02 +0530

    iDB working

--- server/public/script/database.js
+++ server/public/script/database.js
@@ -1,14 +1,42 @@
 window.onload = () => {
     let db;
 
-    const DBOpenRequest = window.indexedDB.open("xpotify", 2);
+    const DBOpenRequest = window.indexedDB.open("xpotify", 3);
+
+    const songs = [
+        {
+            "id" : "2fCklc5HvH4eeu2ilTEvvM",
+            "artists" : "1nonly",
+            "name" : "Shakira!",
+            "audioSrcPath" : "test2.mp3"
+        }
+    ]
 
     DBOpenRequest.onerror = () => {
         console.log("Databse cannot be opened!");
     };
 
-    DBOpenRequest.onsuccess = () => {
+    DBOpenRequest.onsuccess = (event) => {
+        db = event.target.result;
+
         console.log("Database has been opened!");
+
+        const transaction = db.transaction("savedSongs", "readwrite");
+        const objectStore = transaction.objectStore("savedSongs");
+
+        transaction.oncomplete = () => {
+            console.log("Transaction completed!");
+        };
+
+        transaction.onerror = () =>{
+            console.log("Transaction could not be completed!");
+        };
+
+        const request = objectStore.add(songs[0]);
+
+        request.oncomplete = () => {
+            console.log("objectStore operation request fulfilled!");
+        };
     };
 
     DBOpenRequest.onupgradeneeded = (event) => {
@@ -18,9 +46,13 @@ window.onload = () => {
             console.log("Error loading database!");
         };
 
-        const objectStore = db.createObjectStore(["saved songs"], { keyPath: "id"});
+        const objectStore = db.createObjectStore(["savedSongs"], { keyPath: "id"});
+        objectStore.createIndex("artists", "artists", { unique: false });
+        objectStore.createIndex("name", "name", { unique: false});
         objectStore.transaction.oncomplete = () => {
-            console.log("ObjectStore creating completed!");
+            console.log("ObjectStore setting up completed!");
         };
+
+        
     };
 };
