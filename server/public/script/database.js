56bf2e8a3bfe9dd53443508988e7d24f8e7c892f
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-06-22 21:27:24 +0530

    new algo written for playlistTab db api

--- server/public/script/database.js
+++ server/public/script/database.js
@@ -5,22 +5,22 @@
 
     const song = [
         {
-            "id" : "5O0zUvdnJr0RbWzLFneN2i",
-            "artists" : "The Weeknd",
-            "name" : "Popular",
-            "audioSrcPath" : "test4.mp3"
+            "id" : "6FYbr9QzRoZPh0Re8lDO9z",
+            "artists" : "Dean Lewis",
+            "name" : "7 Minutes",
+            "audioSrcPath" : "test7.mp3"
         },
         {
-            "id" : "2z5t8IRRtt5vwkSzP9umJo",
-            "artists" : "Ashely Cooke",
-            "name" : "in your place",
-            "audioSrcPath" : "test5.mp3"
+            "id" : "6w8pFOKn42O418qwcQElZ3",
+            "artists" : "Dean Lewis",
+            "name" : "Waves",
+            "audioSrcPath" : "test8.mp3"
         },
         {
-            "id" : "799XxzSlWNYkuWJxUbuGnF",
-            "artists" : "Metallica",
-            "name" : "Master of Puppets",
-            "audioSrcPath" : "test5.mp3"
+            "id" : "4XJ2SmdXNTkMkkx6PFPU8m",
+            "artists" : "Lewis Capaldi",
+            "name" : "Hold me while you wait",
+            "audioSrcPath" : "test9.mp3"
         }
     ];    
 
@@ -39,7 +39,6 @@
         db = DBOpenRequest.result;
 
         console.log("Database has been opened!");
-        
         
         // saveTrackToDB(song);
         // saveTracksToDB(song);
@@ -132,14 +131,13 @@
         };
     };
 
-
     // const elem = document.getElementById("lewis");
 
     // elem.addEventListener("click", () => {
     //     checkIfPlaylistIsSaved("h1u2h3uk1h31u3h", "playlistMusicTracks");
     // });
 
-    const checkIfPlaylistIsSaved = (playlist, targetElem) => {
+    const checkIfPlaylistIsSaved = (playlist, btn) => {
         // console.log(tracks);
         const transaction = db.transaction(["savedPlaylists"], "readwrite");
         const objectStore = transaction.objectStore("savedPlaylists");
@@ -167,11 +165,11 @@
             
             if(data.length == 1){
                 console.log("Yes! the playlist exists!");
+                btn.src = "/icons/playB.png";
             } else {
                 console.log("No! the playlist does not exist!");
-                addOpacClass(targetElem);
-                // tracks.classList.add("opac");
-            }
+                btn.src = "/icons/download.png";
+            };
         };
 
         request.onerror = () => {
@@ -211,5 +209,43 @@
         };
 
         // console.log("This is it!");
-    }
+    };
+
+    const checkIfTrackIsSaved = async (track, targetElem) => {
+        const transaction = db.transaction(["savedSongs"], "readwrite");
+        const objectStore = transaction.objectStore("savedSongs");
+
+        transaction.oncomplete = () => {
+            console.log("Transaction has been completed!");
+        };
+
+        transaction.onerror = () => {
+            console.log("Transaction could not be completed.");
+        };
+
+        const request = objectStore.get(track);
+
+        request.onsuccess = (event) => {
+            // console.log(request.result);
+            
+            let data = [];
+
+            if(request.result == undefined){
+                // Do nothing;
+            } else {
+                data.push([request.result]);
+            };
+            
+            if(data.length == 1){
+                console.log("Yes! the track exists! TFTF");
+            } else {
+                console.log("No! the track does not exist!");
+                addOpacClass(targetElem);
+            }
+        };
+
+        request.onerror = () => {
+            console.log(request.error);
+        };
+    };
 // };
