fa27def8909580a69832510c86ca9b05c47f02ff
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-09-07 17:34:58 +0530

    f() for loading track on window.load

--- server/public/script/musicPlayerv2.js
+++ server/public/script/musicPlayerv2.js
@@ -22,6 +22,10 @@ if (audio.readyState > 0) {
     });
 }
 
+audio.addEventListener("loadedmetadata", () => {
+    trackDuration[0].innerText = `${calculateTime(audio.duration)}`;
+});
+
 audio.volume = 0.04;
 volSlider.value = audio.volume*100;
 
@@ -46,10 +50,6 @@ audio.addEventListener("timeupdate", () => {
 
 volSlider.addEventListener("input", () => {
     audio.volume = (volSlider.value/100);
-});
-
-audio.addEventListener("loadedmetadata", () => {
-    trackDuration[0].innerText = `${calculateTime(audio.duration)}`;
 });
 
 audioSeeker.addEventListener("change", () => {
@@ -79,4 +79,65 @@ speaker.addEventListener("click", () => {
         speaker.src = "/icons/speakerr.svg"
         volSlider.value = 100
     }
+});
+
+// Main Controls
+
+let q = [
+    {
+        "trackName" : "Guy For That (Feat. Luke Combs)",
+        "trackArtists" : [ 
+            {
+                "artistName" : "Post Malone"
+            },
+            {
+                "artistName" : "Luke Combs"
+            }
+        ],
+        "trackCoverImage" : "https://i.scdn.co/image/ab67616d0000b27340f1c739f1afe06578b62515",
+        "trackSourcePath" : "5crxqqibcvoOQQgg4HjAFQ.mp3"
+    },
+    {
+        "trackName" : "I Had Some Help (Feat. Morgan Wallen)",
+        "trackArtists" : [ 
+            {
+                "artistName" : "Post Malone"
+            },
+            {
+                "artistName" : "Morgan Wallen"
+            }
+        ],
+        "trackCoverImage" : "https://i.scdn.co/image/ab67616d0000b27397306976e3eb8aad53b754eb",
+        "trackSourcePath" : "7221xIgOnuakPdLqT0F3nP.mp3"
+    },
+    {
+        "trackName" : "Worst Way",
+        "trackArtists" : [ 
+            {
+                "artistName" : "Riley Green"
+            },
+            {
+                "artistName" : "Riley Green"
+            }
+        ],
+        "trackCoverImage" : "https://i.scdn.co/image/ab67616d000048513e3cacc30688b39ffed26334",
+        "trackSourcePath" : "1osfLqL6L2iQsirRf83ded.mp3"
+    }
+];
+
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+const trackArtists = document.getElementsByClassName("trackArtists");
+const trackName = document.getElementsByClassName("trackName");
+const trackSmallCoverImage = document.getElementById("trackSmallCoverImg");
+const trackLargeCoverImage = document.getElementById("trackLargeCoverImg");
+
+window.addEventListener("load", () => {
+    trackArtists[0].innerText = `${q[0].trackArtists[0].artistName} & ${q[0].trackArtists[1].artistName}`;
+    trackName[0].innerText = `${q[0].trackName}`;
+    trackSmallCoverImage.src = `${q[0].trackCoverImage}`;
+    trackLargeCoverImage.src = `${q[0].trackCoverImage}`;
+    audio.src = `songs/${q[0].trackSourcePath}`;
+    trackDuration[0].innerText = `${calculateTime(audio.duration)}`;
 });
