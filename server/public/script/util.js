6f63e4f7a7182afac0f62143b1182124450d125f
cr <72098121+CyberRodeo@users.noreply.github.com>
2025-02-23 15:44:31 +0530

    Created util.js
    added more function as utilities for ease in testing.

---
+++ server/public/script/util.js
@@ -0,0 +1,11 @@
+const parseTrackId = async (link) => {
+    return link.slice(31, 53);
+};
+
+const parsePlaylistId = async (link) => {
+    return link.slice(34, 56);
+};
+
+const parseAlbumId = async (link) => {
+    return link.slice(31, 53);
+};
