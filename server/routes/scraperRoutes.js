72d47217fc3b05da1aeb2b3fa87fb54eaa254c77
Shuiii <72098121+CyberRodeo@users.noreply.github.com>
2025-03-29 20:24:00 +0530

    Update scraperRoutes.js
    - Reform for scraper

--- server/routes/scraperRoutes.js
+++ server/routes/scraperRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const puppeteer = require("puppeteer");
 
 const fetchArtistBackground = async (artistId) => {
-    const artistLink = `https://open.spotify.com/artist/1XgFuvRd7r5g0h844A5ZUQ`;
+    const artistLink = `https://open.spotify.com/artist/${artistId}`;
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
@@ -12,33 +12,24 @@ const fetchArtistBackground = async (artistId) => {
     });
 
     const element = await page.$('[data-testid="background-image"');
-    const leti = await page.$('mMx2LUixlnN_Fu45JpFB Ii9XdJaXIuKbmR1zC4Rt Yn2Ei5QZn19gria6LjZj');
-    let letii = [];
+    let backgroundImageLink;
 
-    if(leti.length > 1){
-        for(i=0; i < leti.length; i++){
-            letii.push({i: leti[i].src});
-        };
-    } else {
-        letii = leti;
-    }
-
-    console.log(leti);
 
     if(element){
-        const backgroundImageLink = await page.evaluate(el => el.style.backgroundImage.slice(5, -2), element);
-        console.log(backgroundImageLink);
+        backgroundImageLink = await page.evaluate(el => el.style.backgroundImage.slice(5, -2), element);
         return backgroundImageLink;
     } else {
-        console.log("Background Image link was not found.")
+        console.log("Background Image link was not found.");
+        backgroundImageLink = null;
     }
 
     await browser.close();
+    return backgroundImageLink;
 };
 
 router.get("/artistbg/:id", async (req, res) => {
-    await fetchArtistBackground(req.params.id);
-    res.send("HII LETI!");
+    const arBg = await fetchArtistBackground(req.params.id);
+    res.json(arBg);
 });
 
 module.exports = router;
